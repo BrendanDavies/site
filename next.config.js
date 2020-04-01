43ad2195602a9d371284cdc433427474dfb7a7fa
Davies, Brendan <brendan_davies@comcast.com>
2020-04-01 08:07:58 -0400

    Add service worker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
+const withOffline = require('next-offline');
 
 // next.config.js
-module.exports = withCSS(withImages({}));
+module.exports = withOffline(withCSS(withImages({})));
