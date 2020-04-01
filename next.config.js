e15d1dfe038211f253b48e3692f52bf4252b123f
Davies, Brendan <brendan_davies@comcast.com>
2020-04-01 09:18:32 -0400

    Add workbox config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,33 @@ const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 const withOffline = require('next-offline');
 
+const nextConfig = {
+  target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
+};
+
 // next.config.js
-module.exports = withOffline(withCSS(withImages({})));
+module.exports = withOffline(withCSS(withImages(nextConfig)));
