8f644640302d8d78009e4587855401b480f88fed
Davies, Brendan <brendan_davies@comcast.com>
2020-04-03 21:48:44 -0400

    Add manifest file

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Title } from '../components/Title';
 
 const App = () => (
   <div className="h-screen">
+    <link rel="manifest" href="/manifest.json" />
     <Header></Header>
     <Title></Title>
   </div>
