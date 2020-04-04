20f4d6fe629afbc4b67116ce1ec7fbb61a9f924f
Davies, Brendan <brendan_davies@comcast.com>
2020-04-03 21:58:32 -0400

    Add head element to fetch manifest

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
+import Head from 'next/head';
 import { Header } from '../components/Header';
 import { Title } from '../components/Title';
 
 const App = () => (
-  <div className="h-screen">
-    <link rel="manifest" href="/manifest.json" />
-    <Header></Header>
-    <Title></Title>
-  </div>
+  <>
+    <Head>
+      <title>Brendan Davies</title>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="theme-color" content="#09111C"></meta>
+    </Head>
+    <div className="h-screen">
+      <Header></Header>
+      <Title></Title>
+    </div>
+  </>
 );
 
 export default App;
