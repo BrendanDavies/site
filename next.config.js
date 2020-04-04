88c51a51dc86335e34bc5b45f74d2c6cade79670
Davies, Brendan <brendan_davies@comcast.com>
2020-04-03 21:38:32 -0400

    Try a 3rd thing

--- next.config.js
+++ next.config.js
@@ -3,15 +3,26 @@ const withOffline = require('next-offline');
 
 const nextConfig = {
   target: 'serverless',
-  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  // transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
   // // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
   // // turn on the SW in dev mode so that we can actually test it
   // generateInDevMode: true,
   workboxOpts: {
     runtimeCaching: [
       {
-        urlPattern: /.png$/,
-        handler: 'CacheFirst'
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
       }
     ],
     swDest: 'static/service-worker.js'
