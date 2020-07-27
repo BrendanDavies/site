f04efc01feefa04fc81fb0e9eb16cf3afc789c7e
Davies, Brendan <brendan_davies@comcast.com>
2020-07-27 14:21:49 -0400

    Add other sub pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import Head from 'next/head';
+import Header from '../components/Header.jsx';
 import '../styles/index.css';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Brendan Davies</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192.png"></link>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="theme-color" content="#09111C"></meta>
+      </Head>
+      <Header></Header>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,11 @@
-import Head from 'next/head';
+import Link from 'next/link';
 import { Header } from '../components/Header';
 import { Title } from '../components/Title';
 
-const App = () => (
-  <>
-    <Head>
-      <title>Brendan Davies</title>
-      <link rel="manifest" href="/manifest.json" />
-      <link rel="apple-touch-icon" href="/icon-192.png"></link>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="theme-color" content="#09111C"></meta>
-    </Head>
-    <div className="flex flex-col justify-center h-screen text-center">
-      <Header></Header>
-      <Title></Title>
-    </div>
-  </>
+const Landing = () => (
+  <div className="flex flex-col justify-center h-screen text-center">
+    <Title></Title>
+  </div>
 );
 
-export default App;
+export default Landing;
