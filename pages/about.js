695165e16e7226194746ced225cf491bfb2faf9a
Davies, Brendan <brendan_davies@comcast.com>
2020-08-20 20:23:05 -0400

    Add about blurb

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,15 @@ import { Card } from '../components/Card.tsx';
 const About = () => (
   <Card className="w-4/5 md:w-2/4">
     <h1 className="text-2xl">About</h1>
-    <p>Software Engineer...</p>
+    <p className="m-2">My name is Brendan.</p>
+    <p className="m-2">
+      I am a Software Engineer who has worked across many platforms, from DBA to
+      Frontend Developer, and enjoys the common threads between them.
+    </p>
+    <p className="m-2">
+      In my free time I attempt to be a Woodworker, travel whenever possible,
+      and spend time with my animals when it is not.
+    </p>
   </Card>
 );
 
