ca09a56de9b03297e63aaefd76456aedd900f75a
Davies, Brendan <brendan_davies@comcast.com>
2020-08-27 09:27:38 -0400

    Add new color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   purge: ['./components/**/*.*', './pages/**/*.*'],
   theme: {
+    extend: {
+      colors: {
+        burnt: '#ab4f01',
+        orange: '#f39933',
+        eggplant: '#5b2337'
+        // c4: '#a67871',
+        // c5: '#2e161'
+      }
+    },
     filter: {
       none: 'none',
       grayscale: 'grayscale(1)',
