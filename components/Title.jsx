8d48f40d7eb2d45144c3222649db8b7c677b3b6d
Davies, Brendan <brendan_davies@comcast.com>
2020-10-05 15:56:02 -0400

    Use LinkIcon component for repeating styles

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,27 +1,22 @@
 import GithubIcon from '../images/github-light.svg';
 import ResumeIcon from '../images/resume.svg';
 
+const LinkIcon = ({ Icon, href }) => (
+  <a className="p-3" href={href} target="_blank" rel="noopener noreferrer">
+    <Icon className="w-8 h-8" />
+  </a>
+);
+
 export const Title = () => (
   <div>
     <h1 className="text-4xl">Brendan Davies</h1>
     <p className="m-2 overflow-hidden">Software Engineer</p>
     <span className="flex justify-center m-auto">
-      <a
-        className="p-3"
-        href="https://github.com/BrendanDavies"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <GithubIcon className="w-8 h-8" />
-      </a>
-      <a
-        className="p-3"
+      <LinkIcon href="https://github.com/BrendanDavies" Icon={GithubIcon} />
+      <LinkIcon
         href="https://brendandavies.github.io/resume/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <ResumeIcon className="w-8 h-8" />
-      </a>
+        Icon={ResumeIcon}
+      />
     </span>
   </div>
 );
