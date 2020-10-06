6b003bda69039cb353b8177a004bb341a8028146
Davies, Brendan <brendan_davies@comcast.com>
2020-10-06 11:31:01 -0400

    Use card on title page/give anchor aria label

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,24 +1,37 @@
+import { Card } from '../components/Card.tsx';
+
 import GithubIcon from '../images/github-light.svg';
 import ResumeIcon from '../images/resume.svg';
 
-const LinkIcon = ({ Icon, href }) => (
-  <a className="p-3" href={href} target="_blank" rel="noopener noreferrer">
+const LinkIcon = ({ Icon, href, text }) => (
+  <a
+    aria-label={text}
+    className="p-3"
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
     <Icon className="w-8 h-8" />
   </a>
 );
 
 export const Title = () => (
-  <span className="inline-block p-4 bg-opacity-75 rounded-lg backdrop-blur bg-cover">
+  <Card>
     <h1 className="text-4xl">Brendan Davies</h1>
     <p className="m-2 overflow-hidden">Software Engineer</p>
     <span className="flex justify-center m-auto">
-      <LinkIcon href="https://github.com/BrendanDavies" Icon={GithubIcon} />
       <LinkIcon
+        text="Github"
+        href="https://github.com/BrendanDavies"
+        Icon={GithubIcon}
+      />
+      <LinkIcon
+        text="Resume"
         href="https://brendandavies.github.io/resume/"
         Icon={ResumeIcon}
       />
     </span>
-  </span>
+  </Card>
 );
 
 export default Title;
